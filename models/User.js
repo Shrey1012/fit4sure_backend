fed97677e9e0c3ec39b7e3cf813b3b3a793e156e
Shrey1012 <shrey9157@gmail.com>
2023-07-28 20:46:32 +0530

    Fixed bugs

--- models/User.js
+++ models/User.js
@@ -1,73 +1,6 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-  // name: {
-  //   type: String,
-  // },
-  // mobile_number: {
-  //   type: String,
-  // },
-  // age: {
-  //   type: String,
-  // },
-  // gender: {
-  //   type: String,
-  // },
-  // height: {
-  //   type: String,
-  // },
-  // current_weight: {
-  //   type: String,
-  // },
-  // target_weight: {
-  //   type: String,
-  // },
-  // lifestyle: {
-  //   type: String,
-  // },
-  // medical_condition: {
-  //   type: String,
-  // },
-  // weight_goal: {
-  //   type: String,
-  // },
-  // city: {
-  //   type: String,
-  // },
-  // language: {
-  //   type: String,
-  // },
-  // what_brings: {
-  //   type: String,
-  // },
-  // bio: {
-  //   type: String,
-  // },
-  // image: {
-  //   type: String,
-  // },
-  // diet_preference: {
-  //   type: String,
-  // },
-  // allergies: {
-  //   type: String,
-  // },
-  // preferred_cuisine: {
-  //   type: String,
-  // },
-  // approved: {
-  //   type: Boolean,
-  //   default: false,
-  // },
-  // created_at: {
-  //   type: String,
-  //   default: Date.now,
-  // },
-  // updated_at: {
-  //   type: String,
-  //   default: Date.now,
-  // },
-
   name: {
     type: String,
     required: true,
@@ -79,9 +12,6 @@ const schema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-  },
-  id: {
-    type: String,
   },
   image: {
     type: String,
@@ -98,6 +28,10 @@ const schema = new mongoose.Schema({
   test: {
     type: String,
   },
+  created_at : {
+    type: Date,
+    default: Date.now(),
+  }
 });
 
 module.exports = mongoose.model("User", schema);
